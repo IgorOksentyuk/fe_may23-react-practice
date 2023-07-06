582401cdcf0a44302fc2ca0606df1a45952b982f
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-07-06 11:12:41 +0300

    Linter fixed

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,28 +1,24 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 
-export const Product = ({ product }) => {
-  console.log('product');
+export const Product = ({ product }) => (
+  <tr data-cy="Product">
+    <td className="has-text-weight-bold" data-cy="ProductId">
+      {product.id}
+    </td>
 
-  return (
-    <tr data-cy="Product">
-      <td className="has-text-weight-bold" data-cy="ProductId">
-        {product.id}
-      </td>
+    <td data-cy="ProductName">{product.name}</td>
+    <td data-cy="ProductCategory">
+      {`${product.category.icon} - ${product.category.title}`}
+    </td>
 
-      <td data-cy="ProductName">{product.name}</td>
-      <td data-cy="ProductCategory">
-        {`${product.category.icon} - ${product.category.title}`}
-      </td>
-
-      <td
-        data-cy="ProductUser"
-        className={product.user.sex === 'm'
-          ? 'has-text-link'
-          : 'has-text-danger'
-        }
-      >
-        {product.user.name}
-      </td>
-    </tr>
-  );
-};
+    <td
+      data-cy="ProductUser"
+      className={product.user.sex === 'm'
+        ? 'has-text-link'
+        : 'has-text-danger'
+      }
+    >
+      {product.user.name}
+    </td>
+  </tr>
+);
